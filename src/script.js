0ae8a7f494d1559b3b817574068d55df89129a9f
Pedro Kauan Hentges <kauanhentges40@gmail.com>
2024-06-11 17:52:09 -0300

    Exercício calculadora

--- src/script.js
+++ src/script.js
@@ -1,8 +1,47 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(num1, num2, operador) {
+  let result = "";
 
+  num1 = Number(num1);
+  num2 = Number(num2);
+
+  switch (operador) {
+    case "+":
+      result = num1 + num2;
+      break;
+
+    case "-":
+      result = num1 - num2;
+      break;
+
+    case "*":
+      result = num1 * num2;
+      break;
+
+    case "/":
+      result = num1 / num2;
+      break;
+
+    case "e":
+      let resultExpo = 1;
+      for (let i = 0; i < num2; i++) {
+        resultExpo *= num1;
+      }
+      result = resultExpo;
+      break;
+
+    default:
+      return "ERRO";
+  }
+
+  if (result === undefined || result === 1000000) {
+    return "ERRO";
+  } else {
+    return result;
+  }
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -18,13 +57,12 @@ function limpezatotal() {
 }
 
 function calcular() {
-  let numeros = visor.value.split(/[^0-9]/)
-  let operadores = visor.value.split(/[0-9]/)
-  let operador
-  operadores.forEach(x => {
-    if (x != "")
-      operador = x
+  let numeros = visor.value.split(/[^0-9]/);
+  let operadores = visor.value.split(/[0-9]/);
+  let operador;
+  operadores.forEach((x) => {
+    if (x != "") operador = x;
   });
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  visor.value = Calculadora(numeros[0], numeros[1], operador);
 }
